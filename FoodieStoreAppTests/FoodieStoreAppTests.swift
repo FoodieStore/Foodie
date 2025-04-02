9e329cf1746d0d50b487de0f020c931e72523ded
Selinay Ceylan <selinay.ceylan23@gmail.com>
2025-04-02 21:32:13 +0300

    Initial Commit

---
+++ FoodieStoreAppTests/FoodieStoreAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieStoreAppTests.swift
+//  FoodieStoreAppTests
+//
+//  Created by selinay ceylan on 2.04.2025.
+//
+
+import Testing
+@testable import FoodieStoreApp
+
+struct FoodieStoreAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
